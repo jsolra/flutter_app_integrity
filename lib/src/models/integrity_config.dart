fa081606b30cd53c2f8f0d6cf59ab25e4598445b
jisora <ji.sora@ourhome.co.kr>
2026-06-23 08:39:25 +0900

    feat!: IntegrityConfig에서 콜백/UI 관련 필드 제거
    - ThreatCallback typedef 제거
    - CustomDialogBuilder typedef 제거
    - onThreatDetected 필드 및 생성자 파라미터 제거
    - customDialogBuilder 필드 및 생성자 파라미터 제거
    - package:flutter/widgets.dart import 제거
    - 4개 순수 검증 필드만 유지

--- lib/src/models/integrity_config.dart
+++ lib/src/models/integrity_config.dart
@@ -1,18 +1,3 @@
-import 'package:flutter/widgets.dart';
-
-import 'package:app_integrity/src/models/security_threat.dart';
-
-/// 위협 탐지 시 호출되는 콜백 함수 타입.
-/// [threats]는 탐지된 보안 위협 목록.
-typedef ThreatCallback = void Function(List<SecurityThreat> threats);
-
-/// 커스텀 보안 경고 다이얼로그 빌더 함수 타입.
-/// [context]는 빌드 컨텍스트, [threats]는 탐지된 보안 위협 목록.
-typedef CustomDialogBuilder = Widget Function(
-  BuildContext context,
-  List<SecurityThreat> threats,
-);
-
 /// 앱 무결성 검증 설정을 담는 모델 클래스.
 ///
 /// 호스트 앱에서 앱별 고유 값(서명 해시, 번들 ID)과
@@ -30,18 +15,10 @@ class IntegrityConfig {
   /// 디버그 모드에서 검증 건너뛰기 (기본값: true).
   final bool skipInDebugMode;
 
-  /// 위협 탐지 시 호출되는 콜백.
-  final ThreatCallback? onThreatDetected;
-
-  /// 커스텀 보안 경고 다이얼로그 빌더.
-  final CustomDialogBuilder? customDialogBuilder;
-
   const IntegrityConfig({
     this.validSigningHashes = const [],
     this.validBundleIds = const [],
     this.enableInstallSourceCheck = false,
     this.skipInDebugMode = true,
-    this.onThreatDetected,
-    this.customDialogBuilder,
   });
 }
